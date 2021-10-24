c5ab7c5d875083110e5d8c6d26d68b74f4553dde
calebTree <ande4905@gmail.com>
2021-10-24 18:48:13 -0500

    paging working like normal with react-router-dom

--- src/react.js
+++ src/react.js
@@ -2,32 +2,19 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      home: false,
-      register: false,
-      login: false,
-      hail: false,
-      book: false
-    }
-  }
-
-  handleClick(condition) {
-    this.setState( {home: condition} )
-  }
-
+class Home extends React.Component {
   render() {
-    const condition  = this.state.home;
     return (
       <div>
         <div className="mdl-layout--fixed-header">
           <header className="mdl-layout__header mdl-color-text--white mdl-color--light-blue-700">
             <div className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
-              <div className="mdl-layout__header-row mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop">
-                <h1 onClick={() => this.handleClick(true)}><i className="material-icons">directions_car</i> FAV-RIDE ™</h1>
+              <div className="mdl-layout__header-row mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop">                
+                <h1>
+                  <Link to="/"><i className="material-icons">directions_car</i> FAV-RIDE ™</Link>
+                </h1>                
               </div>
               <div id="user-container">
                 <div hidden id="user-pic"></div>
@@ -42,112 +29,78 @@ class App extends React.Component {
             </div>
           </header>
         </div>
-        {condition === true ? <Welcome /> : <Greeting />}
+        <Route exact path="/" component={Greeting}/>
+        <Route path="/welcome" component={Welcome}/>
+        <Route path="/register" component={Register}/>
+        <Route path="/login" component={Login}/>
       </div>
     )
   }
 }
 
 class Greeting extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      condition: false
-    };
-  }
-
-  handleClick(condition) {
-    this.setState( {condition} )
-  }
-
   render() {
-    const { condition } = this.state;
     return (
       <div className="mdl-layout">
-        {condition === true ? <Welcome /> :
-          <section id="greeting" className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
-            <div className="mdl-card__supporting-text">
-              <h3>Welcome to FAV-RIDE ™</h3>
-              <p className="greeting">FAV-RIDE is the future of transportation using F-ully A-utonomous V-ehicles.</p>
-              <p className="subGreeting">Brought to you by Metro State, Fall 2021 ICS370, Team 2:</p>
-              <p>
-                Caleb Anderson:
-                <a href="mailto:caleb.anderson@my.metrostate.edu">caleb.anderson@my.metrostate.edu</a>
-              </p>
-              <p>
-                Arielle Hounton:
-                <a href="mailto:arielle.hounton@my.metrostate.edu">arielle.hounton@my.metrostate.edu</a>
-              </p>
-              <p>
-                Youssoufou Kante:
-                <a href="mailto:youssoufou.kante@my.metrostate.edu">youssoufou.kante@my.metrostate.edu</a>
-              </p>
-              <p>
-                Jonathan Bracamontes:
-                <a href="mailto:jonathan.bracamontes@my.metrostate.edu">jonathan.bracamontes@my.metrostate.edu</a>
-              </p>
-                <button className="section-button mdl-button mdl-js-button mdl-button--raised" 
-                  onClick={() => this.handleClick(true)}>Continue
-                </button>
+        <section id="greeting" className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
+          <div className="mdl-card__supporting-text">
+            <h3>Welcome to FAV-RIDE ™</h3>
+            <p className="greeting">FAV-RIDE is the future of transportation using F-ully A-utonomous V-ehicles.</p>
+            <p className="subGreeting">Brought to you by Metro State, Fall 2021 ICS370, Team 2:</p>
+            <p>
+              Caleb Anderson:
+              <a href="mailto:caleb.anderson@my.metrostate.edu">caleb.anderson@my.metrostate.edu</a>
+            </p>
+            <p>
+              Arielle Hounton:
+              <a href="mailto:arielle.hounton@my.metrostate.edu">arielle.hounton@my.metrostate.edu</a>
+            </p>
+            <p>
+              Youssoufou Kante:
+              <a href="mailto:youssoufou.kante@my.metrostate.edu">youssoufou.kante@my.metrostate.edu</a>
+            </p>
+            <p>
+              Jonathan Bracamontes:
+              <a href="mailto:jonathan.bracamontes@my.metrostate.edu">jonathan.bracamontes@my.metrostate.edu</a>
+            </p>
+            <div>
+              <Link to="/welcome" className="section-button mdl-button mdl-js-button mdl-button--raised" >Continue</Link>
             </div>
-          </section>
-        }
+          </div>
+        </section>
       </div>
     )
   }
 }
 
 class Welcome extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loggedIn: false,
-      shown: '',
-    };
-  }
-
-  choose(condition) {
-    switch (condition) {
-      case 'registration' :
-        return <Register />;
-      case 'login' :
-        return <Login />;
-      default : 
-        return (
-          <section id="welcome-main" className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">   
-            <div className="home_box">       
-              <div className="hmlogo">
-                <span className="material-icons">directions_car</span>
-              </div>
-              <div className="hmcontent">
-                <p>For all your trip requiring a ride, we will get you there safely.</p>
-              </div>
-              <div id="welcome" className="homeReg">
-                <button onClick={() => this.setState({shown: 'registration'})} id="showRegFormBtn" className="section-button mdl-button mdl-button--raised mdl-button--colored pull-left">Create your account</button>
-                <button onClick={() => this.setState({shown: 'login'})} id="loginFormBtn" className="section-button mdl-button mdl-button--raised mdl-button--accent pull-right">Login</button>
-              </div>
-
-              {/* <!-- Welcome/Main Button Form */}
-              <div id="main-buttons" className="mdl-grid" hidden>
-                <div className="mdl-typography--text-center">
-                  <div className="hmcontent">
-                    <h2>Welcome Back <span hidden className="user-name"></span></h2>
-                  </div>
-                  <a id="scheduleBtn" className="section-button mdl-button mdl-button--raised mdl-button--colored">Schedule A Ride <i className="material-icons">departure_board</i></a>
-                  <a id="hailBtn" className="section-button mdl-button mdl-button--raised mdl-button--accent">Hail A Ride <i className="material-icons">hail</i></a>
-                </div>
-              </div>
-            </div>
-          </section>
-        )
-    }
-  }
-
   render() {
-    const condition = this.state.shown;
     return (
       <div className="mdl-layout">
-        { this.choose(condition) }
+        <section id="welcome-main" className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">   
+          <div className="home_box">       
+            <div className="hmlogo">
+              <span className="material-icons">directions_car</span>
+            </div>
+            <div className="hmcontent">
+              <p>For all your trip requiring a ride, we will get you there safely.</p>
+            </div>
+            <div id="welcome" className="homeReg">
+              <Link to='/register' className="section-button mdl-button mdl-button--raised mdl-button--colored pull-left">Create your account</Link>
+              <Link to='/login' id="loginFormBtn" className="section-button mdl-button mdl-button--raised mdl-button--accent pull-right">Login</Link>
+            </div>
+
+            <div id="main-buttons" className="mdl-grid" hidden>
+              <div className="mdl-typography--text-center">
+                <div className="hmcontent">
+                  <h2>Welcome Back <span hidden className="user-name"></span></h2>
+                </div>
+                <a id="scheduleBtn" className="section-button mdl-button mdl-button--raised mdl-button--colored">Schedule A Ride <i className="material-icons">departure_board</i></a>
+                <a id="hailBtn" className="section-button mdl-button mdl-button--raised mdl-button--accent">Hail A Ride <i className="material-icons">hail</i></a>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     )
   }
@@ -211,27 +164,33 @@ class Login extends React.Component {
 
   render() {
     return (
-      <section id="login" className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
-        <div className="mdl-card__supporting-text">
-          <h3>Login</h3>
-          <form action="#">
-            <div id="name">
-              <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                <input id="loginUsername" className="mdl-textfield__input" type="text" required />
-                <label className="mdl-textfield__label" htmlFor="username">Email / Username</label>
+      <div className="mdl-layout">
+        <section id="login" className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
+          <div className="mdl-card__supporting-text">
+            <h3>Login</h3>
+            <form action="#">
+              <div id="name">
+                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                  <input id="loginUsername" className="mdl-textfield__input" type="text" required />
+                  <label className="mdl-textfield__label" htmlFor="username">Email / Username</label>
+                </div>
+                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                  <input id="loginPass" className="mdl-textfield__input" type="password" required />
+                  <label className="mdl-textfield__label" htmlFor="loginPass">Password</label>
+                </div>
               </div>
-              <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                <input id="loginPass" className="mdl-textfield__input" type="password" required />
-                <label className="mdl-textfield__label" htmlFor="loginPass">Password</label>
-              </div>
-            </div>
-            <button id="loginBtn" className="section-button mdl-button mdl-js-button mdl-button--raised mdl-button--colored pull-left" data-upgraded=",MaterialButton">Submit</button>
-          </form>
-        </div>
-      </section>
+              <button id="loginBtn" className="section-button mdl-button mdl-js-button mdl-button--raised mdl-button--colored pull-left" data-upgraded=",MaterialButton">Submit</button>
+            </form>
+          </div>
+        </section>
+      </div>
     )
   }
 }
 
 const domContainer = document.querySelector('#root');
-ReactDOM.render(<App />, domContainer);
+ReactDOM.render(
+  <BrowserRouter>
+    <Home />
+  </BrowserRouter>
+, domContainer);
